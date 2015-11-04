e6155480af4a28b0bb8a0d6a3175317dbc0b0a79
Nitin Prakash <webcurries@gmail.com>
2015-11-04 16:15:01 +0530

    Updated documentation for config file

--- api/v1/config.php
+++ api/v1/config.php
@@ -2,8 +2,24 @@
 
 include_once ('../vendor/autoload.php');
 
-$pdo = new PDO('mysql:host=localhost;dbname=angular_crud;charset=utf8', 'root', '');
+/*** PDO Object to create database connection
+ *   Replace localhost with your host name in $host = 'localhost';
+ *   Replace angular_crud with your database name in $dbname = 'angular_crud';
+ *   Replace root with your database username in $user = 'root'; 
+ *   Replace '' with your database password in $pass = '';
+ */  
+
+$host = 'localhost';
+$dbname = 'angular_crud';
+$user = 'root';
+$pass = '';
+
+$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+
+/* Initialise NotORM with PDO Details  */
+
 $db = new NotORM($pdo);
 
+/* Initialise Slim Framework Instance */
 $app = new \Slim\Slim();
 
